4cd15f353c76315b96a1fcfac6e84ad5b76ec371
carlosgamer101 <atogocarlos9@gmail.com>
2025-05-06 08:36:22 +0300

    String stuff

---
+++ SString.java
@@ -0,0 +1,25 @@
+public class SString {
+    public static void main(String[] args) {
+        String name ="Strathmore University";
+        System.out.println(name.length());
+        System.out.println(name.substring(3, 7));
+        System.out.println(name.charAt(10));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.indexOf("th"));
+
+        String s1 = "java";
+        String s2 = "Java";
+        boolean eq = s1.equals(s2);
+        System.out.println(eq);
+        String s3 = s1.concat(s2);
+        System.out.println(s3);
+
+        StringBuilder brand = new StringBuilder("Nike");
+        brand.append(" Carl");
+        brand.append(" ");
+        brand.append("World");
+        System.out.println(brand.toString());
+
+    }
+    
+}
